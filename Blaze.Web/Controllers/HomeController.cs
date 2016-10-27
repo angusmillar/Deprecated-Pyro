2cc23545d971ddb05d824394424b1d6f834c912b
Angus Millar <angusmillar@iinet.net.au>
2016-10-27 21:42:58 +1000

    Few more site links and some grammar fixes for the home page

--- Blaze.Web/Controllers/HomeController.cs
+++ Blaze.Web/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Blaze.Web.Controllers
       HomeView.FhirApiEndpoint = "Test.STU3.PyroHealth.net/FhirApi/";
       HomeView.FhirSpecification = new Models.Common.ExternalPageLink()
       {
-        Description = "2016 Sept FHIR V1.6.0",
+        Description = "FHIR September 2016 V1.6.0 specification release",
         Name = "FHIR STU3",
         Url = new Uri("http://hl7.org/fhir/2016Sep/index.html")
       };
@@ -52,6 +52,38 @@ namespace Blaze.Web.Controllers
         Name = "Hay on FHIR",
         Url = new Uri("https://fhirblog.com/")
       });
+
+      HomeView.InformationPageLinkList.Add(new Models.Common.ExternalPageLink()
+      {
+        Description = "HL7 Australia",
+        Name = "HL7 Australia",
+        Url = new Uri("http://www.hl7.org.au/")
+      });
+      
+      HomeView.InformationPageLinkList.Add(new Models.Common.ExternalPageLink()
+      {
+        Description = "Publicly Available FHIR Servers for testing",
+        Name = "Public test FHIR servers",
+        Url = new Uri("http://wiki.hl7.org/index.php?title=Publicly_Available_FHIR_Servers_for_testing")
+      });
+
+      HomeView.InformationPageLinkList.Add(new Models.Common.ExternalPageLink()
+      {
+        Description = "AEGIS FHIR Server testing service",
+        Name = "AEGIS FHIR testing service",
+        Url = new Uri("http://www.aegis.net/touchstone.html")
+      });
+
+      HomeView.InformationPageLinkList.Add(new Models.Common.ExternalPageLink()
+      {
+        Description = "AEGIS FHIR Server testing service",
+        Name = "Crucible FHIR testing service",
+        Url = new Uri("https://projectcrucible.org/")
+      });
+      
+
+
+
 
       return View(HomeView);
     }
