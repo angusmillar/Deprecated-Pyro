c96d10a66b01a691fde5761cad0fe7ec87d6aecd
Angus Millar <angusmilar@inet.net.au>
2016-11-30 22:21:52 +1000

    Finished Basic Integration test case

---
+++ Pyro.Test/IntergrationTest/Test_BasicIntergration.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using NUnit.Framework;
+using Hl7.Fhir.Model;
+
+
+namespace Pyro.Test.IntergrationTest
+{
+  [TestFixture]
+  class Test_BasicIntergration
+  {
+    private string ServerEndPoint = string.Empty;
+    private string FhirEndpoint = string.Empty;
+    private System.IDisposable Server;
+
+    [SetUp]
+    public void Setup()
+    {
+      string LocalHost = "http://localhost";
+      ServerEndPoint = $"{LocalHost}:{Pyro.Common.Web.StaticWebInfo.TestingPort}";
+      FhirEndpoint = $"{LocalHost}:{Pyro.Common.Web.StaticWebInfo.TestingPort}/{Pyro.Common.Web.StaticWebInfo.ServiceRoute}";
+      Server = WebApp.Start<Pyro.Web.Startup>(ServerEndPoint);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      Server.Dispose();
+    }
+
+    [Test]
+    public void TestCreate()
+    {
+      Hl7.Fhir.Rest.FhirClient clientFhir = new Hl7.Fhir.Rest.FhirClient(FhirEndpoint, false);
+      clientFhir.Timeout = 1000 * 480; // give the call a while to execute (particularly while debugging).
+
+      string PatientResourceId = string.Empty;
+
+      //Add a Patient resource by Update
+      Patient PatientOne = new Patient();      
+      PatientOne.Name.Add(HumanName.ForFamily("TestPatient").WithGiven("Test"));
+      PatientOne.Identifier.Add(new Identifier("http://example.org/abn", "1"));
+      PatientOne.Gender = AdministrativeGender.Unknown;
+
+      Patient PatientResult = null;
+      try
+      {
+        PatientResult = clientFhir.Create(PatientOne);
+      }
+      catch (Exception Exec)
+      {
+        Assert.True(false, "Exception thrown on resource Create: " + Exec.Message);
+      }
+      Assert.NotNull(PatientResult, "Resource create by Updated returned resource of null");
+      PatientResourceId = PatientResult.Id;
+      PatientResult = null;
+
+      //Get the Added resource by Id
+      try
+      {
+        //PatientOneResourceId
+        PatientResult = (Patient)clientFhir.Get($"{FhirEndpoint}/Patient/{PatientResourceId}");        
+      }
+      catch (Exception Exec)
+      {
+        Assert.True(false, "Exception thrown on resource Get: " + Exec.Message);
+      }
+      Assert.NotNull(PatientResult, "Resource Get returned resource of null");
+      Assert.AreEqual(PatientResourceId, PatientResult.Id, "Resource created by Updated has incorrect Resource Id");
+      Assert.AreEqual(AdministrativeGender.Unknown, PatientResult.Gender, "Patient gender does not match.");
+
+      //Update
+      PatientResult.Gender = AdministrativeGender.Male;
+      try
+      {        
+        clientFhir.Update(PatientResult);
+      }
+      catch (Exception Exec)
+      {
+        Assert.True(false, "Exception thrown on resource Get: " + Exec.Message);
+      }
+      PatientResult = null;
+
+      //Get the Added resource by Id
+      try
+      {        
+        PatientResult = (Patient)clientFhir.Get($"{FhirEndpoint}/Patient/{PatientResourceId}");
+      }
+      catch (Exception Exec)
+      {
+        Assert.True(false, "Exception thrown on resource Get: " + Exec.Message);
+      }
+      Assert.NotNull(PatientResult, "Resource Get returned resource of null");
+      Assert.AreEqual(AdministrativeGender.Male, PatientResult.Gender, "Patient gender does not match.");
+
+      //Delete Resource
+      try
+      {
+        clientFhir.Delete($"Patient/{PatientResourceId}");
+      }
+      catch (Exception Exec)
+      {
+        Assert.True(false, "Exception thrown on resource Get: " + Exec.Message);
+      }
+
+      //Get the Added resource by Id
+      try
+      {
+        var Result = clientFhir.Get($"{FhirEndpoint}/Patient/{PatientResourceId}");
+      }
+      catch (Hl7.Fhir.Rest.FhirOperationException OpExec)
+      {
+        Assert.AreEqual(OpExec.Status, System.Net.HttpStatusCode.Gone, "Final Get did not return Http Status of Gone.");        
+      }
+      
+
+    }
+  }
+  
+}
