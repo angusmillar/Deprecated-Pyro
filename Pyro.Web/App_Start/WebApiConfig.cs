c1d0bb652626d127942a7bc10c479ab57bb57158
Angus Millar <angusmillar@iinet.net.au>
2017-05-15 14:07:21 +1000

    Enable CORS support

--- Pyro.Web/App_Start/WebApiConfig.cs
+++ Pyro.Web/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace Pyro.Web
 {
@@ -12,6 +13,9 @@ namespace Pyro.Web
   {
     public static void Register(HttpConfiguration config)
     {
+      //Enable CORS
+      config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
       // Web API configuration and services
       // Configure Web API to use only bearer token authentication.
       config.SuppressDefaultHostAuthentication();
