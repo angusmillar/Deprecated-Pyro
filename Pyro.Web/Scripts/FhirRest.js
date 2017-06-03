5dd5c269b09a15db3433c82ac5cc9fe9adf04297
Angus Millar <angusmillar@iinet.net.au>
2017-06-03 22:35:10 +1000

    Using Bloodhound with typeahead

--- Pyro.Web/Scripts/FhirRest.js
+++ Pyro.Web/Scripts/FhirRest.js
@@ -92,40 +92,29 @@ function formatXml(xml) {
 
 
 
-var substringMatcher = function (strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
 
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function (i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
-var states = ['Patient', 'Observation', 'DiagnosticReport', 'Encounter', 'TestOne',
-  'TestTwo', 'TestThree', 'TestFour', 'TestFive', '/','?','&'
-];
 
 $(document).ready(function () {
-$('#the-basics .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-  {
-    name: 'states',
-    source: substringMatcher(states)
+
+  var statesone = ['Patient', 'Observation', 'DiagnosticReport', 'Encounter', 'TestOne',
+    'TestTwo', 'TestThree', 'TestFour', 'TestFive', '/', '?', '&'
+  ];
+
+  // constructs the suggestion engine
+  var states = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: statesone
+  });
+
+  $('#bloodhound .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+    {
+      name: 'states',
+      source: states
     });
 });
