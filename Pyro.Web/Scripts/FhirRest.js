aedb830fa58839aa48b79ed661870e2e8b9bda1d
Angus Millar <angusmillar@iinet.net.au>
2017-06-06 01:22:13 +1000

    Basic FHIR Client Page working

--- Pyro.Web/Scripts/FhirRest.js
+++ Pyro.Web/Scripts/FhirRest.js
@@ -1,54 +1,86 @@
-﻿function OnPageLoad(ServerRootUrl) {
+﻿var _ServerRootUrl = "";
+
+//Make initial FHIR call on page load
+function OnPageLoad(ServerRootUrl) {
   $(document).ready(function () {
-    GetFhirRest(ServerRootUrl, 'json');
-    GetFhirRest(ServerRootUrl, 'xml');
+    _ServerRootUrl = ServerRootUrl;
+    MakeFhirGetCall()    
   });
 }
 
-function GetFhirRest(ServerRootUrl, DataType) {
-  var XmlAcceptValue = 'application/fhir+xml';
-  var JsonAcceptValue = 'application/fhir+json';
-  var AcceptValue = 'application/fhir+json';
+//Make Fhir call on button click
+$(document).ready(function () {  
+  $("#searchButton").click(function () {
+    MakeFhirGetCall();
+  });
+});
 
-  if (DataType == "xml") {
-    AcceptValue = XmlAcceptValue;
-  }
-  else
-  {
-    AcceptValue = JsonAcceptValue;
-  }
+function MakeFhirGetCall()
+{
+  //Get the user query string and append to service root url
+  var FhirQuery = _ServerRootUrl + '/' + $('#queryString').val();
 
-  $.ajax({
-    url: ServerRootUrl + '/Patient/IHIStatusExample',
-    headers: { 'Accept': AcceptValue },
-    type: "GET",
-    dataType: DataType,
-    cache: false,
-    error: function () {
-      if (DataType = 'xml') {
-        $("#fhirjson").text('Error loading FHIR ' + DataType + ' document');
-      }
-      else if (DataType = 'xml') {
-        $("#fhirxml").text('Error loading FHIR ' + DataType + ' document');
-      }
-    },
-    success: SwitchContent
-  }); 
-}
+  //Make the JSON Call
+  var JsonAcceptHeader = 'application/fhir+json';  
+  var jsonrequest = xhr_get(FhirQuery, 'json', JsonAcceptHeader);
 
+  jsonrequest.done(function (data, status, jqxhr) {
+    httpStatusFormated('#fhirjsonStatus', status, jqxhr.status);
+    $("#fhirjson").text(JSON.stringify(data, null, '  '));
+    HighLightBlock();
+  });
 
-function SwitchContent(xml, status, jqxhr) {
-  var ct = jqxhr.getResponseHeader("content-type") || "";
-  if (ct.indexOf('application/fhir+json') > -1) {
+  jsonrequest.fail(function (jqxhr, status, error) {
+    httpStatusFormated('#fhirjsonStatus', status, jqxhr.status);
     var d = jQuery.parseJSON(jqxhr.responseText);
     $("#fhirjson").text(JSON.stringify(d, null, '  '));
-  }
-  if (ct.indexOf('application/fhir+xml') > -1) {
+    HighLightBlock();
+  });
+
+  jsonrequest.always(function (data, status, jqxhr) {
+    //Treat like finaly, clean up if needed.
+    //$("#fhirjson").text("<p>An error has occurred: </p> <p>" + status + "</p>");        
+  });
+  
+  //Make the XML Call
+  var XmlAcceptHeader = 'application/fhir+xml';
+  var xmlrequest = xhr_get(FhirQuery, 'xml', XmlAcceptHeader);
+  
+  xmlrequest.done(function (data, status, jqxhr) {
+    httpStatusFormated('#fhirxmlStatus', status, jqxhr.status);
     $("#fhirxml").text(formatXml(jqxhr.responseText));
-  }   
-  HighLightBlock();
+    HighLightBlock();
+  });
+
+  xmlrequest.fail(function (jqxhr, status, error) {
+    httpStatusFormated('#fhirxmlStatus', status, jqxhr.status);
+    $("#fhirxml").text(formatXml(jqxhr.responseText));
+    HighLightBlock();
+  });
+
+  xmlrequest.always(function (data, status, jqxhr) {
+    //Treat like finaly, clean up if needed.
+    //$("#fhirjson").text("<p>An error has occurred: </p> <p>" + status + "</p>");        
+  });
+
 }
 
+
+function httpStatusFormated(location, statusText, statusCode)
+{
+  return $(location).text("(" + statusCode + ") " + statusText);
+}
+
+
+function xhr_get(FhirQuery, DataType, AcceptHeader) {  
+  return $.ajax({
+    url: FhirQuery,
+    type: 'get',
+    headers: { 'Accept': AcceptHeader },
+    dataType: DataType,
+    cache: false
+  })  
+}
 
 function HighLightBlock() {
 
@@ -89,15 +121,10 @@ function formatXml(xml) {
 }
 
 
-
-
-
-
-
 $(document).ready(function () {
 
-  var statesone = ['Patient', 'Observation', 'DiagnosticReport', 'Encounter', 'TestOne',
-    'TestTwo', 'TestThree', 'TestFour', 'TestFive', '/', '?', '&'
+  var statesone = ['Patient', 'Observation', 'DiagnosticReport', 'Encounter', '$server-resource-report',
+    '$server-indexes-delete-history-indexes'
   ];
 
   // constructs the suggestion engine
@@ -108,6 +135,13 @@ $(document).ready(function () {
     local: statesone
   });
 
+  function extractor(query) {
+    var result = /([^,]+)$/.exec(query);
+    if (result && result[1])
+      return result[1].trim();
+    return '';
+  }
+
   $('#bloodhound .typeahead').typeahead({
     hint: true,
     highlight: true,
@@ -115,6 +149,22 @@ $(document).ready(function () {
   },
     {
       name: 'states',
-      source: states
+      source: states,
+      updater: function(item) {
+        return this.$element.val().replace(/[^,]*$/, '') + item + ',';
+      },
+      matcher: function (item) {
+        var tquery = extractor(this.query);
+        if (!tquery) return false;
+        return ~item.toLowerCase().indexOf(tquery.toLowerCase())
+      },
+      highlighter: function (item) {
+
+        var query = extractor(this.query).replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
+        return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
+          return '<strong>' + match + '</strong>'
+        })
+      }
+
     });
 });
