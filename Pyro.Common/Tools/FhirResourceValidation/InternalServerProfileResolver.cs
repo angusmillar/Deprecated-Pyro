f304709d268b23ffd26de49f1d9c37f87f8393c5
Angus Millar <angusmillar@iinet.net.au>
2017-06-20 08:18:05 +1000

    Start internal validator resolver

---
+++ Pyro.Common/Tools/FhirResourceValidation/InternalServerProfileResolver.cs
@@ -0,0 +1,31 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Specification.Source;
+using System.Collections.Generic;
+using System.Linq;
+using Pyro.Common.Interfaces.Service;
+
+namespace Pyro.Common.Tools.FhirResourceValidation
+{
+  public class InternalServerProfileResolver : IResourceResolver
+  {
+    private _Resource
+    public List<StructureDefinition> TestProfiles = new List<StructureDefinition>();
+
+    public InternalServerProfileResolver(IResourceServices ResourceServices)
+    {
+      ResourceServices.SetCurrentResourceType(FHIRAllTypes.StructureDefinition);
+
+      //Add StructureDefinition resource to this list when we have them rather than obtaining from Server.
+      // TestProfiles.Add();
+    }
+    public Resource ResolveByCanonicalUri(string uri)
+    {
+      return TestProfiles.SingleOrDefault(p => p.Url == uri);
+    }
+
+    public Resource ResolveByUri(string uri)
+    {
+      return ResolveByCanonicalUri(uri);
+    }
+  }
+}
