76abd8967fb5e3fb42641f41de8122db5b4d47ec
Angus Millar <angusmillar@iinet.net.au>
2017-07-10 12:36:07 +1000

    Updated text in error message for setting primary Service root in App_Data\PyroApp.config

--- Pyro.Common/ServiceRoot/RequestServiceRootValidate.cs
+++ Pyro.Common/ServiceRoot/RequestServiceRootValidate.cs
@@ -47,7 +47,7 @@ namespace Pyro.Common.ServiceRoot
         //There is no primary in the database / Cache.
         //If the Web.Config ServiceBaseURL equals the incoming request Service Base URL 
         //Therefore set the database's primary service root URL, as this is a clean install.
-        ILog.Info($"Clean install detected. As the first request's Service root is equal to the ServiceBaseURL found in the Web.config this will be set in the database for future requests. ServiceBaseURL is : {IPrimaryServiceRootCache.GetPrimaryRootUrlFromWebConfig()} ");
+        ILog.Info($"Clean install detected. As the first request's Service root is equal to the ServiceBaseURL found in the App_Data\\PyroApp.config this will be set in the database for future requests. ServiceBaseURL is : {IPrimaryServiceRootCache.GetPrimaryRootUrlFromWebConfig()} ");
         return ICommonServices.SetPrimaryRootUrlStore(WebConfigServiceBase.StripHttp().ToLower());
       }
 
@@ -59,7 +59,7 @@ namespace Pyro.Common.ServiceRoot
         //The incoming request Service Base URL equals the Web.Config entry yet does not equal the Service Base URL
         //found in the database. We will assume someone knows what they are doing if they are capable of changing the Web.Config setting.
         //So update the database primary Service Base URL to match the Web.Config and request
-        ILog.Info($"The incoming request's Service Base URL equals the Web.Config file entry 'ServiceBaseURL' yet does not equal the Primary Service Base URL found in the database. The server will assume someone knows what they are doing if they are capable of changing the Web.Config setting and will update the database primary Service Base URL to match the Web.Config and request. ");
+        ILog.Info($"The incoming request's Service Base URL equals the App_Data\\PyroApp.config file entry 'ServiceBaseURL' yet does not equal the Primary Service Base URL found in the database. The server will assume someone knows what they are doing if they are capable of changing the App_Data\\PyroApp.config setting and will update the database primary Service Base URL to match the App_Data\\PyroApp.config and request. ");
         return ICommonServices.SetPrimaryRootUrlStore(WebConfigServiceBase.StripHttp().ToLower());
       }
 
@@ -84,13 +84,13 @@ namespace Pyro.Common.ServiceRoot
         ErrorMsg =
           $"The incoming Http request had a service root URL of: '{RequestServiceRoot.StripHttp()}'. " +
           $"The server's database service root URL was '{IDtoPrimaryRootUrlStore.Url}'. " +
-          $"The servers Web.Config file service root URL was '{WebConfigServiceBase.StripHttp()}'. " +
+          $"The servers PyroApp.config file service root URL was '{WebConfigServiceBase.StripHttp()}'. " +
           "All three URLs must match for the server to continue! " +
-          "This is most likely due to the server being move from it's original URL location, or the Web.Config value being incorrect. " +
+          "This is most likely due to the server being move from it's original URL location, or the PyroApp.config value being incorrect. " +
           "You need to consider carefully the ramifications of or actions. you take next. " +
           "External systems may have absolute references to FHIR resources in this server " +
           "and changing the primary service root URL may render these external references invalid. " +
-          "What can you do?. In the servers Web.Config appSetttings is a Key names 'ServiceBaseURL'. You can update this to " +
+          "What can you do?. In the servers App_Data\\PyroApp.config appSetttings is a Key names 'ServiceBaseURL'. You can update this to " +
           $"the new service root URL in use. If you change this to equal '{RequestServiceRoot}' the server " +
           "will use this going forward as the service root URL for new resource. Yet all absolute references outside the database " +
           "will become invalid. " +
