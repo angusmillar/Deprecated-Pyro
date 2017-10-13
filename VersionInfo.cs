cfe19e554dbccf1fbbe806b9f0d10e5da0d03ff3
Angus Millar <angusmillar@iinet.net.au>
2017-10-13 09:54:07 +0800

    Aligend Pyro version number with FHRI spec version i.e 3.0.1.build number

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -10,4 +10,7 @@
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.23")]
+//I am going to try and align the Pyro service version with the FHIR spec version, will see how it goes. I can see an issue 
+//if I choose to support two Fhir versions in the one build, will I do that, not sure.
+//So, 3.0.1 is the FHIR spec and then the Pyro Service Build on the end.
+[assembly: AssemblyFileVersion("3.0.1.24")]
