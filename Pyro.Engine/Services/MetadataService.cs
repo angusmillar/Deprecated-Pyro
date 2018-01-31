fa4bb7bddbd0c8de420b36a2983d59ea228de212
Angus Millar <angusmillar@iinet.net.au>
2018-01-31 23:58:07 +0800

    Added start datetime to developer contact phone

--- Pyro.Engine/Services/MetadataService.cs
+++ Pyro.Engine/Services/MetadataService.cs
@@ -67,8 +67,12 @@ namespace Pyro.Engine.Services
       var Contact = new ContactDetail();
       Contact.Name = "Angus Millar";
       Contact.Telecom = new List<ContactPoint>();
-      var PhoneContactPoint = new ContactPoint(ContactPoint.ContactPointSystem.Phone, ContactPoint.ContactPointUse.Mobile, "0481059995");
+      var PhoneContactPoint = new ContactPoint(ContactPoint.ContactPointSystem.Phone, ContactPoint.ContactPointUse.Mobile, "0481 059 995");
+      PhoneContactPoint.Rank = 1;
+      PhoneContactPoint.Period = new Period();
+      PhoneContactPoint.Period.StartElement = new FhirDateTime(new DateTimeOffset(2017, 12, 22, 8, 00, 00, new TimeSpan(8, 00, 00)));      
       Contact.Telecom.Add(PhoneContactPoint);
+
       var EmailContactPoint = new ContactPoint(ContactPoint.ContactPointSystem.Email, ContactPoint.ContactPointUse.Home, "angusbmillar@gmail.com");
       EmailContactPoint.Rank = 1;
       EmailContactPoint.Period = new Period();
