c69b6217852794c35e61a42eef4cb0ea986dc1f4
Angus Millar <angusmillar@iinet.net.au>
2018-03-16 15:52:45 +0800

    Rev version 3.0.1.33 as HI Service work is now all working on Public server.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,4 +13,4 @@
 //I am going to try and align the Pyro service version with the FHIR spec version, will see how it goes. I can see an issue 
 //if I choose to support two Fhir versions in the one build, will I do that, not sure.
 //So, 3.0.1 is the FHIR spec and then the Pyro Service Build on the end.
-[assembly: AssemblyFileVersion("3.0.1.32")]
+[assembly: AssemblyFileVersion("3.0.1.33")]
